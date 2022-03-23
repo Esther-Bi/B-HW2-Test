3bfb3e684773d1745b167f24097609f6b7c501af
Esther Binnes <estherbinnes@gmail.com>
2022-03-23 12:19:19 +0200

    update tests

--- Test.cpp
+++ Test.cpp
@@ -9,7 +9,7 @@ using namespace std;
 TEST_CASE("Good input") {
 	ariel::Notebook notebook;
 	for (int i=0 ; i<20 ; i++){
-		for (int j=0 ; j<50 ; j++){
+		for (int j=0 ; j<20 ; j++){
 			for (int k=0 ; k<50 ; k+=10){
 				CHECK_NOTHROW(notebook.write(i, j, k, ariel::Direction::Horizontal, "abcdefghij"));
 				CHECK_NOTHROW(notebook.read(i, j, k, ariel::Direction::Horizontal, 10));
@@ -23,7 +23,7 @@ TEST_CASE("Negative input") {
 	ariel::Notebook notebook;
 	for (int i=-20 ; i<0 ; i++){
 		for (int j=-20 ; j<10 ; j++){
-			for (int k=-50 ; k<3 ; k+=10){
+			for (int k=-20 ; k<3 ; k+=10){
 				for (int m=-5 ; m<0 ; m++){
 					CHECK_THROWS(notebook.write(i, j, k, ariel::Direction::Horizontal, "abcdefghij"));
 					CHECK_THROWS(notebook.read(i, j, k, ariel::Direction::Vertical, m));
@@ -35,18 +35,27 @@ TEST_CASE("Negative input") {
 }
 
 TEST_CASE("Write to not enough space") {
+	/***
+	write a longer word than the space left to the line
+	***/
 	ariel::Notebook notebook;
 	CHECK_THROWS(notebook.write(0, 1, 95, ariel::Direction::Horizontal, "1234567"));
 	CHECK_THROWS(notebook.write(0, 3, 99, ariel::Direction::Horizontal, "abc"));
 }
 
 TEST_CASE("Delete from not enough space") {
+	/***
+	delete a longer word than the space left to the line
+	***/
 	ariel::Notebook notebook;
 	CHECK_THROWS(notebook.erase(0, 1, 95, ariel::Direction::Horizontal, 7));
 	CHECK_THROWS(notebook.erase(0, 3, 99, ariel::Direction::Horizontal, 3));
 }
 
 TEST_CASE("Write to already written place") {
+	/***
+	write to specipic place in the notebook and then try to write to the same place again
+	***/
 	ariel::Notebook notebook;
 	CHECK_NOTHROW(notebook.write(0, 0, 4, ariel::Direction::Horizontal, "456789"));
 	CHECK_THROWS(notebook.write(0, 0, 7, ariel::Direction::Vertical, "abc"));
@@ -56,18 +65,24 @@ TEST_CASE("Write to already written place") {
 	CHECK_THROWS(notebook.write(2, 3, 4, ariel::Direction::Horizontal, "45678"));
 }
 
-TEST_CASE("Delete from already deleted place") {
+TEST_CASE("Write to  deleted place") {
+	/***
+	delete specipic place in the notebook and then try to write to the same place
+	***/
 	ariel::Notebook notebook;
 	CHECK_NOTHROW(notebook.erase(0, 0, 4, ariel::Direction::Horizontal, 6));
-	CHECK_THROWS(notebook.erase(0, 0, 7, ariel::Direction::Vertical, 3));
+	CHECK_THROWS(notebook.write(0, 0, 7, ariel::Direction::Vertical, "123"));
 	CHECK_NOTHROW(notebook.erase(1, 1, 5, ariel::Direction::Vertical, 5));
-	CHECK_THROWS(notebook.erase(1, 3, 4, ariel::Direction::Horizontal, 3));
+	CHECK_THROWS(notebook.write(1, 3, 4, ariel::Direction::Horizontal, "123"));
 	CHECK_NOTHROW(notebook.erase(2, 0, 7, ariel::Direction::Vertical, 10));
-	CHECK_THROWS(notebook.erase(2, 3, 4, ariel::Direction::Horizontal, 5));
+	CHECK_THROWS(notebook.write(2, 3, 4, ariel::Direction::Horizontal, "12345"));
 }
 
 
 TEST_CASE("Write to unreachable space") {
+	/***
+	columne is more than 100
+	***/
 	ariel::Notebook notebook;
 	CHECK_THROWS(notebook.write(0, 1, 101, ariel::Direction::Vertical, "1234567"));
 	CHECK_THROWS(notebook.write(0, 3, 200, ariel::Direction::Vertical, "abc"));
@@ -76,6 +91,9 @@ TEST_CASE("Write to unreachable space") {
 }
 
 TEST_CASE("Delete from unreachable space") {
+	/***
+	columne is more than 100
+	***/
 	ariel::Notebook notebook;
 	CHECK_THROWS(notebook.erase(0, 1, 101, ariel::Direction::Vertical, 15));
 	CHECK_THROWS(notebook.erase(0, 3, 200, ariel::Direction::Vertical, 1));
@@ -84,14 +102,15 @@ TEST_CASE("Delete from unreachable space") {
 }
 
 TEST_CASE("Undefined chars in string input") {
+	/***
+	defined chars are above 32 and under 125 (including both)
+	***/
 	string word;
 	ariel::Notebook notebook;
 	for (int i=0 ; i<32 ; i++){
 		word = i;
 		CHECK_THROWS(notebook.write(0, 1, 3, ariel::Direction::Vertical, word));
 	}
-	word = 95;
-	CHECK_THROWS(notebook.write(0, 3, 7, ariel::Direction::Vertical, word));
 	for (int i=126 ; i<200 ; i++){
 		word = i;
 		CHECK_THROWS(notebook.write(0, 1, 3, ariel::Direction::Vertical, word));
@@ -99,6 +118,9 @@ TEST_CASE("Undefined chars in string input") {
 }
 
 TEST_CASE("Write more than 100 chars horizontally") {
+	/***
+	the string is too long for the line
+	***/
 	string word;
 	for (int i=0 ; i<101 ; i++){
 		word = word + "a";
@@ -108,6 +130,37 @@ TEST_CASE("Write more than 100 chars horizontally") {
 }
 
 TEST_CASE("Delete more than 100 chars horizontally") {
+	/***
+	the requested string is too long
+	***/
 	ariel::Notebook notebook;
-	CHECK_THROWS(notebook.erase(0, 1, 101, ariel::Direction::Horizontal, 101));
+	CHECK_THROWS(notebook.erase(0, 1, 3, ariel::Direction::Horizontal, 101));
+}
+
+TEST_CASE("Write more than 100 chars vertically") {
+	/***
+	the string is not blocked
+	***/
+	string word;
+	for (int i=0 ; i<101 ; i++){
+		word = word + "a";
+	}
+	ariel::Notebook notebook;
+	CHECK_NOTHROW(notebook.write(0, 1, 0, ariel::Direction::Vertical, word));
+}
+
+TEST_CASE("Delete more than 100 chars vertically") {
+	/***
+	the requested string is not blocked
+	***/
+	ariel::Notebook notebook;
+	CHECK_NOTHROW(notebook.erase(0, 1, 3, ariel::Direction::Vertical, 101));
+}
+
+TEST_CASE("Actual good outputs") {
+	ariel::Notebook notebook;
+	notebook.write(0, 1, 0, ariel::Direction::Horizontal, "hello");
+	CHECK(notebook.read(0, 1, 0, ariel::Direction::Horizontal, 5) == "hello");
+	notebook.write(3, 1, 0, ariel::Direction::Vertical, "hello goodbye");
+	CHECK(notebook.read(3, 1, 0, ariel::Direction::Vertical, 5) == "hello goodbye");
 }
